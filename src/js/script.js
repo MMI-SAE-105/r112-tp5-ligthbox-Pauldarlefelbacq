7fa9055d4c1c89ecd4cac351254bad8fdc7c57c8
Pauldarlefelbacq <paul.darle-felbacq@edu.univ-fcomte.fr>
2024-12-04 09:01:55 +0100

    js

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 // *** LIGHTBOX ***
+const lightbox = document.querySelector("#lightbox");
+const imglist = document.querySelectorAll('ul li img');
+const lightboxImg = lightbox.querySelector("img");
+
+imglist.forEach((imglist) => {
+    imglist.addEventListener('click', () => {
+        const imgsrc = event.target.dataset.imgsrc;
+        lightbox.showModal();
+    });
+});
+
